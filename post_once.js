b21af74bfed26fbe9bf226de182ad06478cddbd8
anjou3 <98035474+anjou3@users.noreply.github.com>
2026-04-15 13:41:46 +0900

    Update post_once.js

--- post_once.js
+++ post_once.js
@@ -6,7 +6,12 @@ const path = require("path");
 const TWEETS_FILE = path.join(__dirname, "tweets.json");
 const LOG_FILE    = path.join(__dirname, "posted.log");
 
-const client = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
+const client = new TwitterApi({
+  appKey:       process.env.API_KEY,
+  appSecret:    process.env.API_SECRET,
+  accessToken:  process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_TOKEN_SECRET,
+});
 
 const rwClient = client.readWrite;
 
